36bbbf89367bd0a3b6a5ceeebe711bccf3ac6811
Alamjeet Singh Outlook <alamjeet.singh@outlook.com>
2025-12-31 02:42:51 +0000

    Fix TypeScript JSX namespace error in blog content

--- app/blog/blogContent.tsx
+++ app/blog/blogContent.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import Link from 'next/link';
 
-export const blogContent: Record<string, { content: JSX.Element }> = {
+export const blogContent: Record<string, { content: React.ReactElement }> = {
     'how-to-find-ip-address': {
         content: (
             <>
